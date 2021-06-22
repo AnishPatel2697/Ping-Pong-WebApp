b457fe4ac2666c72ee4ee599c8514cd1521ff374
USCS-XXX <USCS-XXX@scs.com>
2021-06-22 16:39:26 -0700

    Ping Pong Final App

---
+++ ScoreKeeper.js
@@ -0,0 +1,10 @@
+
+const btns = body.querySelector('button');
+btns.addEventListener('click',function(){
+    bton.style.backgroundColor = "green";
+})
+// for (bton of btns) {
+//     bton.addEventListener('click',function(){
+//         bton.style.backgroundColor = "green";
+//     })
+// }
